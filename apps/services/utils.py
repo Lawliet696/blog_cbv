98e0c4b3eb4c47fe92a089b80d739b6ed2ddc2cd
Lawliet696 <Lawliet69696@yandex.ru>
2024-10-05 12:25:32 +0300

    Blog CBV

---
+++ apps/services/utils.py
@@ -0,0 +1,12 @@
+from uuid import uuid4
+from pytils.translit import slugify
+
+
+def unique_slugify(instance, slug, slug_field):
+    model = instance.__class__
+    unique_slug = slug_field
+    if not slug_field:
+        unique_slug = slugify(slug)
+    elif model.objects.filter(slug=slug_field) and model.objects.filter(slug=slug_field).last().id != instance.id:
+        unique_slug = f'{slugify(slug)}-{uuid4().hex[:8]}'
+    return unique_slug
